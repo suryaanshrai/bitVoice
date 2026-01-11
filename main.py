825fe73a44e7063c5836d8d64c11735e1d8d0469
Suryaansh Rai <suryaanshrai@gmail.com>
2026-01-11 21:16:06 +0530

    changes

--- main.py
+++ main.py
@@ -1,15 +1,95 @@
+from datetime import datetime
+
+start_time = datetime.now()
 
 import torchaudio as ta
 from chatterbox.tts import ChatterboxTTS
+import re
+from markdown import markdown
+from bs4 import BeautifulSoup
+import torch
 
-# English example
-model = ChatterboxTTS.from_pretrained(device="cuda")
 
-text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
-wav = model.generate(text)
-ta.save("test-english.wav", wav, model.sr)
+CHUNK_LENGTH = 200
+SETTINGS = {
+    "audio_prompt_path": "silent patient book review.wav",
+    "exaggeration": 0.8, # [0.25 - 2]
+    "cfg_weight": 0.2, # [0.02 - 1]
+    "temperature": 0.3, # [0.05 - 5]
+}
 
-# If you want to synthesize with a different voice, specify the audio prompt
-# AUDIO_PROMPT_PATH = "YOUR_FILE.wav"
-# wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
-# ta.save("test-2.wav", wav, model.sr)
+
+
+def split_text(text, title=None):
+    chunks = re.split(r'(?<=[.?!])\s*', text)
+    result = []
+    i = 0
+    while i < len(chunks):
+        chunk = chunks[i]
+        while i < len(chunks) - 1 and len(chunk) + len(chunks[i+1]) < CHUNK_LENGTH:
+            chunk += " " + chunks[i+1]
+            i += 1
+        result.append(chunk)
+        i += 1
+    if title:
+        result = [title] + result
+    return result
+
+def read_dir():
+    pass
+
+
+def generate_audio(text, dest, settings):
+    model = ChatterboxTTS.from_pretrained(device="cuda")
+    audio_segments = []
+
+    for para in text:
+        if para.strip():  
+            print(f"\n\nGENERATING CHUNK of size {len(para)}. CHUNK content: {para}")            
+            wav = model.generate(para, **settings)
+            audio_segments.append(wav)
+    
+    combined_wav = torch.cat(audio_segments, dim=-1)
+    ta.save(dest, combined_wav, model.sr)
+
+def clean_md(md_content):
+    md_content = re.sub(r'^---\s*\n.*?\n---\s*\n', '', md_content, flags=re.DOTALL)
+    html_content = markdown(md_content)
+    soup = BeautifulSoup(html_content, 'html.parser')
+    text = soup.get_text()
+    return text
+
+
+with open("Reading a bit.md", "r") as f:
+    content = f.read()
+    poem = clean_md(content)
+
+chunks = split_text(poem)
+generate_audio(text=chunks[:2], dest="reading_a_bit_surya.wav", settings=SETTINGS)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+duration = datetime.now() - start_time
+print(f"\n\nTIME TAKEN TO EXECUTE: {duration}")
